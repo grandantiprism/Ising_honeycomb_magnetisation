4260c08f5200b75e051c288792a833f587359cc1
ADE <ackielilac.g1@gmail.com>
2026-01-12 11:59:07 +0900

    parametre fine-tuned

--- Ising_honeycomb_magnetisation/main.cpp
+++ Ising_honeycomb_magnetisation/main.cpp
@@ -13,13 +13,13 @@ using namespace std;
 namespace fs = std::filesystem;
 
 // シミュレーションパラメータの設定
-const int L = 16;            // 格子サイズ (L x L)
+const int L = 128;            // 格子サイズ (L x L)
 const int N = L * L;        // 全スピン数
 const int MCS = 1000000;      // 各温度でのモンテカルロステップ数
 const int THERM = (L < 64) ? 1000 : L * 20;     // 熱平衡化のための捨てステップ数
 
-const double beta_min = 0.6;
-const double beta_max = 0.7;
+const double beta_min = 0.635;
+const double beta_max = 0.685;
 const int num_beta = 20;
 
 struct Ising2D {
